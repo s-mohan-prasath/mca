a404aaa6fd237583e50dc0402ad2e4853e2c2146
24mx115 <24mx115@MCA.PSG>
2024-09-24 10:20:02 +0530

    added dsa

---
+++ dsa/Main.java
@@ -0,0 +1,48 @@
+import java.util.*;
+public class Main{
+    public static void main(String[] arg){
+        problems4 p = new problems4();
+        
+//        PROBLEM 1
+
+        // ArrayList<Integer> l = new ArrayList<Integer>();
+        // l.add(100);
+        // l.add(200);
+        // l.add(31);
+        // l.add(13);
+        // l.add(97);
+        // l.add(10);
+        // l.add(20);
+        // l.add(11);
+        // p.prob1(l);
+
+//         PROBLEM 2
+
+        // int[] arr = {1,2,3,4,5,6,7,8};
+        // int len=8,k=4;
+        // prob2(len,k,arr);
+
+//        PROBLEM 3
+
+//        prob3(new int[]{1, 2, 3, 4, 5, 4, 9, 0});
+
+//        PROBLEM 5
+        // int[] arr = {1,0,0,2,1,0,2,2};
+        // p.prob5(arr);
+
+//        PROBLEM 6
+
+        // int[] arr = {1,2,-2,10,-16,77,0,-55};
+        // p.prob6(arr);
+
+//        PROBLEM 7
+
+        // p.prob7("Good to see you today!");
+        // p.prob7ii("nice");
+
+//        PROBLEM 8
+
+        // p.prob8("GooD");
+
+    }
+}
---
+++ dsa/problems4.java
@@ -0,0 +1,192 @@
+import java.util.ArrayList;
+
+public class problems4 {
+    public problems4() {
+
+    }
+
+    public void prob1(ArrayList<Integer> arr) {
+        ArrayList<Integer> output = new ArrayList<Integer>();
+        int n = arr.size();
+        int m = 0;
+        while (m < n) {
+            int i = 2;
+            int x = arr.get(m);
+            while (i < n) {
+                if (x % i == 0) {
+                    output.add(x);
+                    break;
+                }
+                i++;
+            }
+            m++;
+        }
+        System.out.println(output.toString());
+    }
+
+    public void prob2(int len, int k, int[] arr) {
+        ArrayList<Integer> output = new ArrayList<Integer>();
+        int i;
+        for (i = k; i < len; i++) {
+            output.add(arr[i]);
+        }
+        for (i = 0; i < k; i++) {
+            output.add(arr[i]);
+        }
+        System.out.println(output.toString());
+    }
+
+    public void prob3(int[] arr) {
+        int n = arr.length;
+        if (n < 3) {
+            if (n == 0) {
+                System.out.println(-1);
+            } else if (n == 1) {
+                System.out.println(0);
+            } else {
+                if (arr[0] > arr[1])
+                    System.out.println(0);
+                else
+                    System.out.println(1);
+            }
+            return;
+        } else {
+            int i = 1;
+            while (i < n - 1) {
+                if (arr[i] > arr[i - 1] && arr[i] > arr[i + 1]) {
+                    System.out.println(i);
+                    return;
+                }
+                i++;
+            }
+            System.out.println(-1);
+        }
+    }
+
+    public void prob4(int[] arr) {
+
+    }
+    public void prob5(int[] arr){
+        int zeros,ones,twos,len,i;
+        zeros = 0;
+        ones = 0;
+        twos = 0;
+        len = arr.length;
+        for(i = 0;i<len;i++){
+            if(arr[i]==0)zeros++;
+            else if(arr[i]==1)ones++;
+            else twos++;
+        }
+        for(i=0;i<len;i++){
+            if(zeros!=0){
+                arr[i] = 0;
+                zeros--;
+            }else if(ones!=0){
+                arr[i] = 1;
+                ones--;
+            }else{
+                arr[i] = 2;
+                twos--;
+            }
+        }
+        printArray(arr);
+    }
+    
+    public void prob6(int[] arr){
+        int len,i,positives=0,pi,ni,ele;
+        len = arr.length;
+        ArrayList<Integer> custom = new ArrayList<>();
+        i = 0;
+        while(i<len){
+            custom.add(0);
+            if(arr[i]>=0){
+                positives++;
+            }
+            i++;
+        }
+        pi = 0;
+        ni = positives;
+        i = 0;
+        while(i<len){
+            ele = arr[i];
+            if(ele>=0){
+                custom.set(pi, ele);
+                pi++;
+            }else{
+                custom.set(ni,ele);
+                ni++;
+            }
+            i++;
+        }
+        i = 0;
+        while(i<len){
+            arr[i] = custom.get(i);
+            i++;
+        }
+        printArray(arr);
+    }
+    
+    public void prob7(String str){
+        int len,i,count;
+        len = str.length();
+        
+        count = 0;
+        i = 0;
+        while(i<len){
+            char c = str.charAt(i);
+            if(c=='a' || c=='e' || c=='i' || c=='o' || c=='u'){
+                count++;
+            }
+            i++;
+        }
+        System.out.printf("Number of Vowels in the String is %d",count);
+    }
+
+    public void prob7ii(String str){
+        int len;
+        String reverseStr,newStr;
+        reverseStr = new String("");
+        newStr = new String(str);
+        len = 0;
+        try{
+            while(true){
+                str.charAt(len);
+                len++;
+            }
+        }catch(Exception e){
+            System.out.println("Length of the String is "+len);
+        }finally{
+            len = len-1;
+            while(len>=0){
+                reverseStr+=str.substring(len,len+1);
+                len--;
+            }
+            System.out.println("Reverse String is "+reverseStr);
+            System.out.println("New String is "+newStr);
+        }
+    }
+
+    public void prob8(String str){
+        int len;
+        String output = new String("");
+        len = str.length();
+        int i = 0;
+        while(i<len){
+            char c = str.charAt(i);
+            int ascii = (int)c;
+            if(ascii>=97 && ascii<=122){
+                ascii-=32;
+            }
+            output+=Character.toString((char)ascii);
+            i++;
+        }
+        System.out.println("Output : "+output);
+    }
+
+    public void printArray(int[] arr){
+        int len = arr.length;
+        for(int i = 0;i<len;i++){
+            System.out.print(arr[i]+"\t");
+        }
+    }
+}
