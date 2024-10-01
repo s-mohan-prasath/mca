8b035ff112751961e800d1a9fdcfc96af7e50835
24mx115 <24mx115@MCA.PSG>
2024-10-01 17:48:39 +0530

    c_ps5 exercise problem1 workout

--- c lab/c-problems-5/main.c
+++ c lab/c-problems-5/main.c
@@ -8,7 +8,7 @@ void prob1();
 
 int main()
 {
-    printf("Hello world!\n");
+    prob1();
     return 0;
 }
 
@@ -22,19 +22,64 @@ following tasks to reorganize the warehouse:
 2. Remove Duplicates: Ensure that each item ID appears only once in the final list.
 3. Sort Items: Sort the item IDs in ascending order
     */
-    int i,len;
-    getArrayLen(&len);
-    int arr[len];
-    getArray(len,arr);
+    int i,j,k,len;
 
     int section1[] = {101,102,103,101};
     int section2[] = {103,104,105};
     int section3[] = {106,107,102};
 
     int len1,len2,len3;
-    len1 = sizeof(section1)/sizeof(arr[0]);
-    len2 = sizeof(section2)/sizeof(arr[0]);
-    len3 = sizeof(section3)/sizeof(arr[0]);
+    len1 = sizeof(section1)/sizeof(section1[0]);
+    len2 = sizeof(section2)/sizeof(section2[0]);
+    len3 = sizeof(section3)/sizeof(section3[0]);
+    len = len1+len2+len3;
+    int merged[len];
+
+    printf("Consolidate Sections: Merge multiple sections into a single list of item IDs.\n");
+    int noOfArr = 0;
+    i = 0;
+    k = 0;
+    while(i<len1){
+        merged[k] = section1[i];
+        k++;
+        i++;
+    }
+    i = 0;
+    while(i<len2){
+        merged[k] = section2[i];
+        i++;
+        k++;
+    }
+    i = 0;
+    while(i<len3){
+        merged[k] = section3[i];
+        k++;
+        i++;
+    }
+    printArray(len,merged);
+
+    int end1=0,end2=0;
+    int temp[len];
+    int noduplicate[len];
+    i=0;
+    k=0;
+    while(i<len){
+        ele = merged[i];
+        j = 0;
+        while(j<end1){
+            if(ele==temp[j]){
+                i++;
+                continue;
+            }
+            j++;
+        }
+        noduplicate[end2] = ele;
+        temp[end1] = ele;
+        end1++;
+        end2++;
+        i++;
+    }
+
 }
 
 void getArrayLen(int *len)
