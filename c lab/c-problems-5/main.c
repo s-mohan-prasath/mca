a950d3f9d072f261a63c5b1bb8ee3c7ef4ad4bce
24mx115 <24mx115@MCA.PSG>
2024-10-24 10:19:22 +0530

    work on c lab probsheet 5

--- c lab/c-problems-5/main.c
+++ c lab/c-problems-5/main.c
@@ -5,10 +5,14 @@ void getArray(int,int * arr);
 void getArrayLen(int*len);
 //TODO: work on prob1
 void prob1();
+void prob2();
+void prob3();
+void sortArray(int len,int *arr);
+void findArrayStat(int len,int * arr,int *maxi,int *mini,int *sum,float *avg);
 
 int main()
 {
-    prob1();
+    prob3();
     return 0;
 }
 
@@ -27,59 +31,85 @@ following tasks to reorganize the warehouse:
     int section1[] = {101,102,103,101};
     int section2[] = {103,104,105};
     int section3[] = {106,107,102};
+}
 
-    int len1,len2,len3;
-    len1 = sizeof(section1)/sizeof(section1[0]);
-    len2 = sizeof(section2)/sizeof(section2[0]);
-    len3 = sizeof(section3)/sizeof(section3[0]);
-    len = len1+len2+len3;
-    int merged[len];
+void prob2(){
+    int len;
+    getArrayLen(&len);
+    int arr[len];
+    getArray(len,arr);
+    int k;
+    printf("Enter the gift type to find : ");
+    scanf("%d",&k);
 
-    printf("Consolidate Sections: Merge multiple sections into a single list of item IDs.\n");
-    int noOfArr = 0;
-    i = 0;
-    k = 0;
-    while(i<len1){
-        merged[k] = section1[i];
-        k++;
-        i++;
-    }
-    i = 0;
-    while(i<len2){
-        merged[k] = section2[i];
-        i++;
-        k++;
-    }
-    i = 0;
-    while(i<len3){
-        merged[k] = section3[i];
-        k++;
-        i++;
-    }
-    printArray(len,merged);
-
-    int end1=0,end2=0;
-    int temp[len];
-    int noduplicate[len];
-    i=0;
-    k=0;
+    int count = 0;
+    int i =0;
     while(i<len){
-        ele = merged[i];
-        j = 0;
-        while(j<end1){
-            if(ele==temp[j]){
-                i++;
-                continue;
-            }
-            j++;
-        }
-        noduplicate[end2] = ele;
-        temp[end1] = ele;
-        end1++;
-        end2++;
+        if(arr[i]==k)count++;
         i++;
     }
+    int output[count];
+    i = 0;
+    while(i<count){
+        output[i] = k;
+        i++;
+    }
+    printf("Days with Gift type %d is ",k);
+    printArray(count,output);
+    printf("Occurrences : %d",count);
 
+}
+
+void prob3(){
+    printf("\nStudent Grade\n");
+    int len;
+    getArrayLen(&len);
+    int arr[len];
+    getArray(len,arr);
+    int maxi,mini,sum;
+    float avg;
+    findArrayStat(len,arr,&maxi,&mini,&sum,&avg);
+    printf("Average Grade : %f\n",avg);
+    printf("Maximum Grade : %d\n",maxi);
+    sortArray(len,arr);
+    printf("Sorted Grade : ");
+    printArray(len,arr);
+}
+
+void findArrayStat(int len,int *arr,int *maxi,int *mini,int *sum,float *avg){
+    int maxiii,miniii,lsum;
+    maxiii = arr[0];
+    miniii = arr[0];
+    lsum = 0;
+    int i = 0;
+    while(i<len){
+        if(arr[i]>maxiii){
+            maxiii = arr[i];
+        }
+        else if(arr[i]<miniii){
+            miniii = arr[i];
+        }
+        lsum+=arr[i];
+        i++;
+
+    }
+    *mini = miniii;
+    *maxi = maxiii;
+    *sum = lsum;
+    *avg = ((float)lsum/len);
+}
+
+void sortArray(int len,int *arr){
+    int temp;
+    for(int i = 0;i<len;i++){
+        for(int j = i;j<len;j++){
+            if(arr[i]>arr[j]){
+                temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
 }
 
 void getArrayLen(int *len)
@@ -99,7 +129,6 @@ void getArray(int len, int *arr)
 
 void printArray(int len, int *arr)
 {
-    printf("Elements are :\n");
     for (int i = 0; i < len; i++)
     {
         printf("%d ", arr[i]);
